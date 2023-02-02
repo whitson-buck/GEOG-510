24dc68ab8e0c0d50abbf976ca7592012afb44e7f
whitson-buck <whitsonbuck@gmail.com>
2023-02-02 10:40:48 -0500

    yessir

--- test.py
+++ test.py
@@ -1,4 +1,13 @@
-a=[3,5,3,5,4]
+def forwardorback(lists):
+    userinput = input("Forward or reverse? [f]/r: ")
+    if userinput != "f" or "r":
+        print("Unexpected character. Try again.")
+    elif userinput == "f":
+        for i in range(len(lists)):
+            print(lists[i])
+    else:
+        for i in range(len(lists)):
+            print(lists[::-i])
 
-for i in range(len(a)):
-    print(a[i])
+
+forwardorback([3,5,6,3])
